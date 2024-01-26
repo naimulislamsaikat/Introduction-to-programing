5991cb3b48dc10cfaa25403d7231891beabd68bc
Naimul Islam <129820411+naimulislamsaikat@users.noreply.github.com>
2024-01-26 21:48:55 +0600

    Basic

---
+++ for loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int i=0,n;
+    for(n=1; n<=200; n=n+1)
+    {
+        i=i+n;
+        cout<<"Loop is= "<<n<<endl;
+    }
+    return 0;
+}
