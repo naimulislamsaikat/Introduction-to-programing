392ebb70849d6db94d0aae3ce0a43cb7c236727b
Naimul Islam <129820411+naimulislamsaikat@users.noreply.github.com>
2024-04-23 10:19:35 +0600

    Add files via upload

---
+++ Calculate the Factorial of a number.cpp
@@ -0,0 +1,20 @@
+/*Calculate the Factorial of a number.
+Sample Output:
+Your input to calculate factorial: 5
+Factorial of 5 is 120 */
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n,factorial=1,i;
+    cout<<"Your input to calculate factorial= ";
+    cin>>n;
+    for(i=1; i<=n; i++)
+    {
+        factorial=factorial*i;
+    }
+    cout<<"Factorial of the number is = "<<factorial<<endl;
+    return 0;
+}
