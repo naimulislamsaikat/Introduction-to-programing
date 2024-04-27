34d29c88fe6ba60e73a22740aa6a920b6e6245bf
Naimul Islam <129820411+naimulislamsaikat@users.noreply.github.com>
2024-04-27 18:53:06 +0600

    User input

--- Fibonicci.cpp
+++ Fibonicci.cpp
@@ -1,14 +1,13 @@
 /*Find the Fibonacci series of N terms.
 Sample Output:
-Your input is: 9
+Your input is: 8
 Fibonacci Series: 0 1 1 2 3 5 8 13 21*/
 
 #include<iostream>
 using namespace std;
-int main()
-{
+int main(){
     int N,v1,v2,i,sum;
-    cout<<"Enter any number : ";
+    cout<<"Enter the range for count till: ";
     cin>>N;
     cout<<"Enter the 1st number that you want to see as : ";
     cin>>v1;
@@ -16,13 +15,11 @@ int main()
     cin>>v2;
     cout<<v1<<" "<<v2;
 
-    for(i=2; i<N; i++)
-    {
+    for(i=2; i<=N; i++){
         sum=v1+v2;
         v1=v2;
         v2=sum;
-
-    cout<<" "<<sum;
+        cout<<" "<<sum;
     }
-    return 0;
+return 0;
 }
