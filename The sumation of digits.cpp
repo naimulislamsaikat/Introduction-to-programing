faa82267c28976e67b696fa90c1be85671326989
Naimul Islam <129820411+naimulislamsaikat@users.noreply.github.com>
2024-06-27 00:21:34 +0600

    User Input

--- The sumation of digits.cpp
+++ The sumation of digits.cpp
@@ -3,19 +3,15 @@
 using namespace std;
 
 int main(){
-
-int n,i,sum=0;
+ int n,i,sum=0;
  cout<<"Enter the Number : " ;
-   cin>>n;
-
-    while(n>0)
-{
-    i=n%10;
-    sum=sum+i;
-    n=n/10;
-
+ cin>>n;
+ 
+ while(n>0){
+  i=n%10;
+  sum=sum+i;
+  n=n/10;
 }
-
  cout<<"\nThe summation of all digits is :" <<sum<<endl;
 return 0;
 }
