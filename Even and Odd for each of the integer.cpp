219c30e7f1e6c853c7937be8667e2f4942c10531
Naimul Islam <129820411+naimulislamsaikat@users.noreply.github.com>
2024-07-08 20:13:15 +0600

    User Input

--- Even and Odd for each of the integer.cpp
+++ Even and Odd for each of the integer.cpp
@@ -2,25 +2,21 @@
 
 #include<iostream>
 using namespace std;
-int main()
-{
+int main(){
     int N;
     cout<<"Enter the value: ";
     cin>>N;
     int num[N],reminder;
-    for(int i=0; i<N; i++)
-    {
+    for(int i=0; i<N; i++){
         cout<<"Enter the number: ";
         cin>>num[i];
     }
-    for(int i=0; i<N; i++)
-    {
+    for(int i=0; i<N; i++){
         reminder=num[i]%2;
-        if (reminder==0)
-        {
+        if (reminder==0){
             cout<<"Even: "<<num[i]<<endl;
         }
         else cout<<"Odd: "<<num[i]<<endl;
     }
-    return 0;
+return 0;
 }
