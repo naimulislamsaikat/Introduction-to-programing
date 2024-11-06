225378f275644ad9d9ec39fb58b0e37dc8160c9b
Naimul Islam <129820411+naimulislamsaikat@users.noreply.github.com>
2024-11-06 21:16:25 +0600

    Check if the array is unique or not if not unique then cancel all repetition.

---
+++ Unique_Array.cpp
@@ -0,0 +1,36 @@
+//find the array is unique or not
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int array[10];
+    int array2[10];
+    int i,j,k = 0;
+    cout << "The array can store 10 digits." <<endl;
+    cout << "Enter the elements: " <<endl;
+    for (int i = 0; i < 10; i++){
+        cin >> array[i];
+    }
+
+    for( i=0; i<10; i++ ){
+        for( j=i+1; j<10; j++ ){
+            if( array[i] == array[j] ){
+                break;
+            }
+        }
+        if( j == 10 ){
+            array2[k] = array[i];
+            k++;
+        }
+    }
+    if( k == 10 ){
+        cout << "Already unique.";
+    }
+    else{
+        for( i=0; i<k; i++ )
+            cout << array2[i] << " ";
+    }
+
+return 0;
+}
